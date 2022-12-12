d6c937fc759b6dd482fc78a6806f54d30a45a081
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2022-12-12 21:00:16 +0200

    added sort direction functions

--- JS/filterSortDirection.js
+++ JS/filterSortDirection.js
@@ -1,23 +1,55 @@
-function searchSortOptionDown() {
-	let buttonDown = document.getElementsByClassName("searchSortOptionDown").item(0);
-	buttonDown.style.backgroundColor = "#33ccff";
-    let buttonUp = document.getElementsByClassName("searchSortOptionUp").item(0);
-	buttonUp.style.backgroundColor = "white";
-	filterSortDirectionDown();
-}
-
-function searchSortOptionUp() {
-	let buttonUp = document.getElementsByClassName("searchSortOptionUp").item(0);
-	buttonUp.style.backgroundColor = "#33ccff";
-	let buttonDown = document.getElementsByClassName("searchSortOptionDown").item(0);
+function addSortDirectionOptionStyleColorBtn(){
+	let buttonDown = document.getElementsByClassName("searchSortDirectionOptionDown").item(0);
+	let buttonUp = document.getElementsByClassName("searchSortDirectionOptionUp").item(0);
 	buttonDown.style.backgroundColor = "white";
-    filterSortDirectionUp();
+	buttonUp.style.backgroundColor = "#33ccff";
 }
 
-function filterSortDirectionDown(){
-    
+function searchSortDirectionOptionDown() {
+	let buttonDown = document.getElementsByClassName("searchSortDirectionOptionDown").item(0);
+	let buttonUp = document.getElementsByClassName("searchSortDirectionOptionUp").item(0);
+
+	if (buttonDown.style.backgroundColor == "white") {
+		let char_container = document.getElementsByClassName("char_container");
+		let char = document.getElementsByClassName("char");
+		let temp_char = [];
+		let charLength = char.length;
+		for (let i = 0; i < charLength; i++) {
+			temp_char[i] = char.item(i);
+		}
+		char_container.item(0).innerHTML = "";
+		decrease_char_length = charLength - 1;
+		for (let i = 0; i < charLength; i++) {
+			char_container.item(0).appendChild(temp_char[decrease_char_length]);
+			decrease_char_length = decrease_char_length - 1;
+		}
+
+	}
+
+	buttonDown.style.backgroundColor = "#33ccff";
+	buttonUp.style.backgroundColor = "white";
 }
 
-function filterSortDirectionUp(){
-    
+function searchSortDirectionOptionUp() {
+	let buttonUp = document.getElementsByClassName("searchSortDirectionOptionUp").item(0);
+	let buttonDown = document.getElementsByClassName("searchSortDirectionOptionDown").item(0);
+
+	if (buttonUp.style.backgroundColor == "white") {
+		let char_container = document.getElementsByClassName("char_container");
+		let char = document.getElementsByClassName("char");
+		let temp_char = [];
+		let charLength = char.length;
+		for (let i = 0; i < charLength; i++) {
+			temp_char[i] = char.item(i);
+		}
+		char_container.item(0).innerHTML = "";
+		decrease_char_length = charLength - 1;
+		for (let i = 0; i < charLength; i++) {
+			char_container.item(0).appendChild(temp_char[decrease_char_length]);
+			decrease_char_length = decrease_char_length - 1;
+		}
+	}
+
+	buttonUp.style.backgroundColor = "#33ccff";
+	buttonDown.style.backgroundColor = "white";
 }
