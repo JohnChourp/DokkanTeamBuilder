a338b1cb390533fdaf5549d0f5d68555f04b28bb
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2022-12-15 19:35:39 +0200

    added DIsplay filter

--- JS/filterDisplay.js
+++ JS/filterDisplay.js
@@ -1,9 +1,3 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function searchDisplayOptions() {
-	document.getElementsByClassName("dropdown-options").item(0).classList.toggle("show");
-}
-
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
 	if (!event.target.matches('.selected-dropdown')) {
@@ -17,3 +11,20 @@ window.onclick = function (event) {
 		}
 	}
 }
+
+// When the user clicks on the button, toggle between hiding and showing the dropdown content
+function searchDisplayOptions() {
+	document.getElementsByClassName("dropdown-options").item(0).classList.toggle("show");
+	//add class when clicked
+}
+
+function addDropdownClass(filterDisplay) {
+	let dropdownOptionsValue = document.getElementsByClassName("dropdown-options-value");
+	dropdownOptionsValue.item(filterDisplay).classList.add("checkedDisplayBtn");
+	for (let i = 0; i < dropdownOptionsValue.length; i++) {
+		if (i != filterDisplay) {
+			dropdownOptionsValue.item(i).classList.remove("checkedDisplayBtn");
+		}
+	}
+}
+
