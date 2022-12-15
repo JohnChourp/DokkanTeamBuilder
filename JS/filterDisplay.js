63f0e8663f13b227b505a1f0ec8af9ed8f36f66d
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2022-12-15 21:00:03 +0200

    added other working display filter options

--- JS/filterDisplay.js
+++ JS/filterDisplay.js
@@ -19,8 +19,10 @@ function searchDisplayOptions() {
 }
 
 function addDropdownClass(filterDisplay) {
+	let selecteddropdown_btn = document.getElementsByClassName("selected-dropdown").item(0);
 	let dropdownOptionsValue = document.getElementsByClassName("dropdown-options-value");
 	dropdownOptionsValue.item(filterDisplay).classList.add("checkedDisplayBtn");
+	selecteddropdown_btn.innerHTML = dropdownOptionsValue.item(filterDisplay).innerHTML;
 	for (let i = 0; i < dropdownOptionsValue.length; i++) {
 		if (i != filterDisplay) {
 			dropdownOptionsValue.item(i).classList.remove("checkedDisplayBtn");
@@ -30,26 +32,66 @@ function addDropdownClass(filterDisplay) {
 	let dataCharName = 'data-char-name';
 	let dataCharTitle = 'data-char-title';
 	let dataCharId = 'data-char-id';
+	let dataCharHp = 'data-char-hp';
+	let dataCharAttack = 'data-char-attack';
+	let dataCharDefense = 'data-char-defense';
+	let dataCharCost = 'data-char-cost';
+	let dataCharRecruit = 'data-char-recruit';
+	let dataCharRelease = 'data-char-release';
 	let dataCharNameItems = document.querySelectorAll('[' + dataCharName + ']');
 	let dataCharTitleItems = document.querySelectorAll('[' + dataCharTitle + ']');
 	let dataCharIdItems = document.querySelectorAll('[' + dataCharId + ']');
+	let dataCharHpItems = document.querySelectorAll('[' + dataCharHp + ']');
+	let dataCharAttackItems = document.querySelectorAll('[' + dataCharAttack + ']');
+	let dataCharDefenseItems = document.querySelectorAll('[' + dataCharDefense + ']');
+	let dataCharCostItems = document.querySelectorAll('[' + dataCharCost + ']');
+	let dataCharRecruitItems = document.querySelectorAll('[' + dataCharRecruit + ']');
+	let dataCharReleaseItems = document.querySelectorAll('[' + dataCharRelease + ']');
 	let char_display = document.getElementsByClassName("char_display");
 
-	if(filterDisplay == 1){
+	if (filterDisplay == 0) {
 		for (let i = 0; i < char_display.length; i++) {
 			char_display.item(i).innerHTML = dataCharNameItems[i].getAttribute(dataCharName);
 		}
 	}
-	if(filterDisplay == 2){
+	if (filterDisplay == 1) {
 		for (let i = 0; i < char_display.length; i++) {
 			char_display.item(i).innerHTML = dataCharTitleItems[i].getAttribute(dataCharTitle);
 		}
 	}
-	if(filterDisplay == 3){
+	if (filterDisplay == 2) {
 		for (let i = 0; i < char_display.length; i++) {
 			char_display.item(i).innerHTML = dataCharIdItems[i].getAttribute(dataCharId);
 		}
 	}
-
+	if (filterDisplay == 3) {
+		for (let i = 0; i < char_display.length; i++) {
+			char_display.item(i).innerHTML = dataCharHpItems[i].getAttribute(dataCharHp);
+		}
+	}
+	if (filterDisplay == 4) {
+		for (let i = 0; i < char_display.length; i++) {
+			char_display.item(i).innerHTML = dataCharAttackItems[i].getAttribute(dataCharAttack);
+		}
+	}
+	if (filterDisplay == 5) {
+		for (let i = 0; i < char_display.length; i++) {
+			char_display.item(i).innerHTML = dataCharDefenseItems[i].getAttribute(dataCharDefense);
+		}
+	}
+	if (filterDisplay == 6) {
+		for (let i = 0; i < char_display.length; i++) {
+			char_display.item(i).innerHTML = dataCharCostItems[i].getAttribute(dataCharCost);
+		}
+	}
+	if (filterDisplay == 7) {
+		for (let i = 0; i < char_display.length; i++) {
+			char_display.item(i).innerHTML = dataCharRecruitItems[i].getAttribute(dataCharRecruit);
+		}
+	}
+	if (filterDisplay == 8) {
+		for (let i = 0; i < char_display.length; i++) {
+			char_display.item(i).innerHTML = dataCharReleaseItems[i].getAttribute(dataCharRelease);
+		}
+	}
 }
-
