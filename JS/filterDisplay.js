be465f865903fd13bb7b26db8eda599a8c8a6d29
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2022-12-15 20:12:23 +0200

    added working options for display filter

--- JS/filterDisplay.js
+++ JS/filterDisplay.js
@@ -26,5 +26,30 @@ function addDropdownClass(filterDisplay) {
 			dropdownOptionsValue.item(i).classList.remove("checkedDisplayBtn");
 		}
 	}
+
+	let dataCharName = 'data-char-name';
+	let dataCharTitle = 'data-char-title';
+	let dataCharId = 'data-char-id';
+	let dataCharNameItems = document.querySelectorAll('[' + dataCharName + ']');
+	let dataCharTitleItems = document.querySelectorAll('[' + dataCharTitle + ']');
+	let dataCharIdItems = document.querySelectorAll('[' + dataCharId + ']');
+	let char_display = document.getElementsByClassName("char_display");
+
+	if(filterDisplay == 1){
+		for (let i = 0; i < char_display.length; i++) {
+			char_display.item(i).innerHTML = dataCharNameItems[i].getAttribute(dataCharName);
+		}
+	}
+	if(filterDisplay == 2){
+		for (let i = 0; i < char_display.length; i++) {
+			char_display.item(i).innerHTML = dataCharTitleItems[i].getAttribute(dataCharTitle);
+		}
+	}
+	if(filterDisplay == 3){
+		for (let i = 0; i < char_display.length; i++) {
+			char_display.item(i).innerHTML = dataCharIdItems[i].getAttribute(dataCharId);
+		}
+	}
+
 }
 
