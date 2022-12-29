46e97e5c3c180fff3562c3637dd1306c4622f9c7
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2022-12-29 20:17:04 +0200

    added filters to sidebar

--- JS/filters/filterRarity.js
+++ JS/filters/filterRarity.js
@@ -1,8 +1,14 @@
 function addRarity(rarityFilter) {
     if (document.getElementById(rarityFilter).classList.contains("checkedRarityBtn")) {
         document.getElementById(rarityFilter).classList.remove("checkedRarityBtn");
+        document.getElementById(rarityFilter).children.item(0).style.backgroundColor = "#6B6B67";
+        document.getElementById(rarityFilter).children.item(1).style.backgroundColor = "#555555";
+        document.getElementById(rarityFilter).children.item(8).style.backgroundImage = 'linear-gradient(180deg, #B6B6B6, #948D87)';
     } else {
         document.getElementById(rarityFilter).classList.add("checkedRarityBtn");
+        document.getElementById(rarityFilter).children.item(0).style.backgroundColor = "#2DA9DD";
+        document.getElementById(rarityFilter).children.item(1).style.backgroundColor = "#1B79C3";
+        document.getElementById(rarityFilter).children.item(8).style.backgroundImage = 'linear-gradient(180deg, #FFFFFF,#DBCEBD)';
     }
 }
 
