0ce6a6b045b912b7e617962e02892a11371901ce
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2023-01-12 19:22:43 +0200

    moved titles search from sidenav to main page

--- JS/search/searchTitles.js
+++ JS/search/searchTitles.js
@@ -1,14 +1,18 @@
-function searchTitlesOptionYes() {
-	let buttonYes = document.getElementById("searchTitlesOptionYes");
-	let buttonNo = document.getElementById("searchTitlesOptionNo");
+function searchTitlesYes() {
+	let buttonYes = document.getElementsByClassName("searchTitles").item(0);
+	let buttonNo = document.getElementsByClassName("searchTitles").item(1);
 	buttonYes.classList.add("checkedTitlesnBtn");
 	buttonYes.style.backgroundColor = "#33ccff";
 	buttonNo.style.backgroundColor = "white";
+	searchCharNameOrTitle(2);
+	searchCharName(2);
 }
-function searchTitlesOptionNo() {
-	let buttonYes = document.getElementById("searchTitlesOptionYes");
-	let buttonNo = document.getElementById("searchTitlesOptionNo");
+function searchTitlesNo() {
+	let buttonYes = document.getElementsByClassName("searchTitles").item(0);
+	let buttonNo = document.getElementsByClassName("searchTitles").item(1);
 	buttonYes.classList.remove("checkedTitlesnBtn");
 	buttonYes.style.backgroundColor = "white";
 	buttonNo.style.backgroundColor = "#33ccff";
+	searchCharNameOrTitle(1);
+	searchCharName(1);
 }
