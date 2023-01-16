7b5487238511e7522947c9264ed30c9e42ca9a49
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2023-01-16 21:19:35 +0200

    add settings button to show settings

---
+++ JS/searchSettings/searchSettings.js
@@ -0,0 +1,11 @@
+function searchSettings(){
+	let searchSettingsId = document.getElementById("search-settings-id");
+	let searchSettingsImageId = document.getElementById("search-settings-image-id");
+	if(searchSettingsImageId.classList.contains("checkedSettingsBtn")){
+		searchSettingsImageId.classList.remove("checkedSettingsBtn");
+		searchSettingsId.setAttribute("style", "display:none;");
+	}else{
+		searchSettingsImageId.classList.add("checkedSettingsBtn");
+		searchSettingsId.setAttribute("style", "display:block;");
+	}
+}
