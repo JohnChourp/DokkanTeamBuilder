e615b0f284b8f348c6dd4db3e523e8115c466e46
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2023-01-25 22:54:07 +0200

    fix file in analytics

--- JS/googleAnalytics.js
+++ JS/googleAnalytics.js
@@ -1,6 +1,11 @@
-function googleAnalytics(){
-	window.dataLayer = window.dataLayer || [];
-	function gtag() { dataLayer.push(arguments); }
+window.dataLayer = window.dataLayer || [];
+
+function gtag() { 
+	dataLayer.push(arguments); 
+}
+
+function googleAnalytics() {
+	
 	gtag('js', new Date());
 	gtag('config', 'G-S015YVBQ1D');
 }
