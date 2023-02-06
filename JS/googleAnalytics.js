b733e41a557b2f10d56669d28b9adfedbdf15a9a
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2023-02-06 16:03:41 +0200

    fixing google analytics

--- JS/googleAnalytics.js
+++ JS/googleAnalytics.js
@@ -1,6 +1,10 @@
+window.dataLayer = window.dataLayer || [];
+
+function gtag() { 
+	dataLayer.push(arguments); 
+}
+
 function googleAnalytics(){
-	window.dataLayer = window.dataLayer || [];
-	function gtag() { dataLayer.push(arguments); }
 	gtag('js', new Date());
 	gtag('config', 'G-S015YVBQ1D');
 }
