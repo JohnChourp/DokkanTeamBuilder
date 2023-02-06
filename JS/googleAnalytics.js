baeea6b82e88cb8df4f11e5c1abd64353b4b6faa
IOANNIS CHOURPOULIADIS <60551435+JohnChourp@users.noreply.github.com>
2023-02-06 19:40:41 +0200

    fixing google analytics

--- JS/googleAnalytics.js
+++ JS/googleAnalytics.js
@@ -1,21 +1,6 @@
-// window.dataLayer = window.dataLayer || [];
-
-// function gtag() { 
-// 	dataLayer.push(arguments); 
-// }
-
-// function googleAnalytics(){
-// 	gtag('js', new Date());
-// 	gtag('config', 'G-S015YVBQ1D');
-// }
-
-var imported = document.createElement('script');
-imported.src = 'https://www.googletagmanager.com/gtag/js?id=G-S015YVBQ1D';
-document.head.appendChild(imported);
-
-
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-
-gtag('config', 'G-S015YVBQ1D');
+function googleAnalytics() {
+	window.dataLayer = window.dataLayer || [];
+	function gtag() { dataLayer.push(arguments); }
+	gtag('js', new Date());
+	gtag('config', 'G-S015YVBQ1D');
+}
