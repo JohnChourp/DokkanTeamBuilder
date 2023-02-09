c27fa4ff46607e46e97fc9b95feca0c5de784012
IOANNIS CHOURPOULIADIS <jonhiedarn@gmail.com>
2023-02-09 12:24:16 +0200

    fixing analytics

--- JS/googleAnalytics.js
+++ JS/googleAnalytics.js
@@ -1,9 +1,9 @@
 function loadAnalytics() {
-    const importScript = document.createElement("script");
-    importScript.src = "https://www.googletagmanager.com/gtag/js?id=G-CS0302XFKQ";
+    var importScript = document.createElement('script');
+    importScript.src = 'https://www.googletagmanager.com/gtag/js?id=G-CS0302XFKQ';
     document.head.appendChild(importScript);
     window.dataLayer = window.dataLayer || [];
-    const gtag = () => dataLayer.push(arguments);
-    gtag("js", new Date());
-    gtag("config", "G-CS0302XFKQ");
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-S015YVBQ1D');
 }
