56eec2185ee5a8818c4ae39dbbb22116a213c561
IOANNIS CHOURPOULIADIS <jonhiedarn@gmail.com>
2023-03-01 11:41:35 +0200

    bug fix cookies in tag manager

--- JS/cookiesBoxPrompt.js
+++ JS/cookiesBoxPrompt.js
@@ -13,6 +13,8 @@ function cookiesBox() {
     const cookiesAcceptReject = localStorage.getItem("cookiesAcceptReject");
     if (cookiesAcceptReject === "accept") {
         cookiesPrompt.style.display = "none";
+        const tagManager = document.getElementById("tag-manager-id");
+        tagManager.innerHTML = "<noscript><iframe src='https://www.googletagmanager.com/ns.html?id=GTM-TBW6L4T' height='0' width='0' style='display:none;visibility:hidden'></iframe></noscript>";
         loadAnalytics();
     } else {
         cookiesPrompt.style.display = "block";
